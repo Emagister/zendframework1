8daf79697c0648aabd121ca7874465838aa66578
Christian <csoronellas@emagister.com>
2012-11-16 08:26:47 +0100

    Fixed scripts to strip require_once

--- bin/strip-zf-requires-mac.sh
+++ bin/strip-zf-requires-mac.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+find ../src/Zend -name '*.php' | grep -v './Loader/Autoloader.php' | xargs sed -E -i~ 's/(require_once)/\/\/ \1/g'
+find ../src/Zend -name '*.php~' | xargs rm -f
 find ../src/ZendX -name '*.php' | grep -v './Loader/Autoloader.php' | xargs sed -E -i~ 's/(require_once)/\/\/ \1/g'
 find ../src/ZendX -name '*.php~' | xargs rm -f
--- bin/strip-zf-requires.sh
+++ bin/strip-zf-requires.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/sh
 find ../src/Zend -name '*.php' -not -wholename '*/Loader/Autoloader.php' -not -wholename '*/Application.php' -print0 | xargs -0 sed --regexp-extended --in-place 's/(require_once)/\/\/ \1/g'
+find ../src/ZendX -name '*.php' -not -wholename '*/Loader/Autoloader.php' -not -wholename '*/Application.php' -print0 | xargs -0 sed --regexp-extended --in-place 's/(require_once)/\/\/ \1/g'
